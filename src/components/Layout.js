0fffb62f25906aa8e55cb02adb346a2d2775e1a1
Bryan Balderrama <bryxn.alex77@gmail.com>
2023-05-09 19:46:03 -0600

    Added layout and Material UI

---
+++ src/components/Layout.js
@@ -0,0 +1,17 @@
+import { Container } from "@mui/material";
+import Header from "./Header";
+import Footer from "./Footer";
+
+
+export default function Layout ({ children }) {
+  return (
+    <>
+     <Header></Header>
+     <Container fixed>
+        <main>{children}</main>
+
+     </Container>
+     <Footer/>
+    </>
+  )
+}
